4bee12596528ef7a9a477607dabe637db9cbf1c3
Fariz Muhammad <muhafariz@gmail.com>
2026-03-12 03:26:25 +0700

    - add profile photo - enable api logging on non prod - integrate json resume using gist resume.json - clean up unused files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  logging: {
+    fetches: {
+      fullUrl: process.env.NODE_ENV !== 'production',
+      hmrRefreshes: process.env.NODE_ENV !== 'production',
+    },
+  },
 };
 
 export default nextConfig;
