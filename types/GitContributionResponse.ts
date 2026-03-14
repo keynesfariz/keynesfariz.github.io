a25f1c52cabe08d38e618e66025fecc3ddcc300b
Fariz Muhammad <muhafariz@gmail.com>
2026-03-14 14:25:32 +0700

    Update types

---
+++ types/GitContributionResponse.ts
@@ -0,0 +1,13 @@
+type GitContribution = {
+  date: string;
+  count: number;
+  level: 0 | 1 | 2 | 3 | 4;
+};
+
+export default interface GitContributionResponse {
+  total: {
+    [year: number]: number;
+    [year: string]: number; // 'lastYear;
+  };
+  contributions: Array<GitContribution>;
+}
---
+++ types/Writing.ts
@@ -0,0 +1,9 @@
+export default interface Writing {
+  title: string;
+  created_at: string;
+  updated_at?: string;
+  url: string;
+  tags: string[];
+  description: string;
+  content: string;
+}
