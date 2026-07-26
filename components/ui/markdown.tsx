3bab2e3b9c8a7ecd21d246f5c66b74308f7b300a
Muhammad Fariz <muhafariz@gmail.com>
2026-07-26 10:39:42 +0700

    refactor: reorganize import statements in writing-list and markdown components

--- components/ui/markdown.tsx
+++ components/ui/markdown.tsx
@@ -1,11 +1,12 @@
+import rehypeHighlight from 'rehype-highlight';
+import ReactMarkdown from 'react-markdown';
+import React from 'react';
+
 import type { Components } from 'react-markdown';
 
 import { extractImageDimensions } from '@/lib/image.server';
-import rehypeHighlight from 'rehype-highlight';
 import 'highlight.js/styles/github-dark.css';
-import ReactMarkdown from 'react-markdown';
 import { slugify } from '@/lib/string';
-import React from 'react';
 
 const extractText = (node: React.ReactNode): string => {
   if (typeof node === 'string') return node;
